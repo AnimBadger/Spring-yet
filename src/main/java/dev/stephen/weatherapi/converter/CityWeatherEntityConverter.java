0e17c1faa48d3c9299cc2da2b365295f43c6b157
AnimBadger <anim.ansah.stephen@gmail.com>
2024-03-10 15:21:10 +0000

    deleted all converters

--- src/main/java/dev/stephen/weatherapi/converter/CityWeatherEntityConverter.java
+++
@@ -1,9 +0,0 @@
-package dev.stephen.weatherapi.converter;
-
-import dev.stephen.weatherapi.model.response.WeatherResponse;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CityWeatherEntityConverter {
-
-}
